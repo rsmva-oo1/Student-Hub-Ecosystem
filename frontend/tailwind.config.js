acbdaed98c56e0e59f01a001cd667492c7e95ad2
Munisa <munisa.rsmvao1@gmail.com>
2026-05-19 22:59:21 +0500

    update tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+ 
+  darkMode: 'class', 
+
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}", 
@@ -8,7 +10,7 @@ export default {
     extend: {
       colors: {
         slate: {
-          850: '#1e293b', // B
+          850: '#1e293b', 
         }
       }
     },
